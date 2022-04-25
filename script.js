b7273621dd8e41a63addfbefe32816ce880788e2
Nak14 <nickkraut@gmail.com>
2022-04-25 15:10:24 -0700

    Bug: readySetGo only logs 'tie'
    I attempted to make my logic statements more concise,
    but I will now attempt to just do them the long way
    then work to simplify them in the future.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //One user: user1 versus the computer: cpu1
 //Let user input 'rock' 'paper' 'or scissors'
-//make sure lower or upper case works
+//make sure lower or upper case && spaces before/after works
 function userSelection(){
 let user1Input = prompt('Enter rock, paper, or scissors');
-user1Input = user1Input.toLocaleLowerCase();
+user1Input = user1Input.trim().toLocaleLowerCase();
 if (user1Input == 'rock' || 
     user1Input === 'paper' ||
     user1Input == 'scissors'){
 return user1Input}
+//What to print if user enters an invalid string?
 else return console.log('Try again');
 }
 console.log(userSelection());
 
-//What to print if user enters an invalid string?
 
 //randomly generate cpu1 selection between rock, paper, or scissors
 function computerSelection(){
@@ -35,3 +35,24 @@ function computerSelection(){
    
 //compare user1Input and cpuOutput r > s, s > p, p > r
 //print who won
+function readySetGo(userSelection,computerSelection){
+   //options  if user wins
+    if ((userSelection === 'rock' 
+        && computerSelection === 'scissors') ||
+        (userSelection === 'scissors' 
+        && computerSelection === 'paper') ||
+        (userSelection === 'paper' 
+        && computerSelection === 'rock')){
+        return ('You win!' /*${userSelection} beats ${computerSelection}'*/);
+    }
+    //if user loses to cpu
+    else if ((computerSelection === 'rock' 
+    && userSelection === 'scissors') ||
+    (computerSelection === 'scissors' 
+    && userSelection === 'paper') ||
+    (computerSelection === 'paper' 
+    && userSelection === 'rock')){
+    return ('You lose!' /*${userSelection} beats ${computerSelection}'*/);
+}
+    else {return 'tie!';}
+    } console.log(readySetGo());
